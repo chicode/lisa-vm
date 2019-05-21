d557e151554db3f201187038dbe1cf98f0660591
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-05-20 23:17:54 -0500

    Add '=' function

--- src/stdlib.ts
+++ src/stdlib.ts
@@ -1,4 +1,13 @@
-import { Value, bool, BoolValue, none } from "./values";
+import {
+  Value,
+  bool,
+  BoolValue,
+  none,
+  NoneValue,
+  ListValue,
+  StrValue,
+  NumValue
+} from "./values";
 import { LisaError } from "./error";
 import { LocatedNode } from "./ast";
 
@@ -18,12 +27,43 @@ const notnone = native(
   }
 );
 
-const log = native((loc, ...args) => {
-  console.log(...args.map(arg => arg[0]));
-  return none();
-});
+const log = native(
+  (_loc, ...args): NoneValue => {
+    console.log(...args.map(arg => arg[0]));
+    return none();
+  }
+);
+
+export function equals(lhs: Value, rhs: Value): boolean {
+  if (lhs.type !== rhs.type) return false;
+  switch (lhs.type) {
+    case "none":
+      return true;
+    case "bool":
+    case "str":
+    case "num":
+      return lhs.value === (rhs as BoolValue | StrValue | NumValue).value;
+    case "list":
+      const arrL = lhs.value;
+      const arrR = (rhs as ListValue).value;
+      return (
+        arrL.length === arrR.length &&
+        arrL.every((elem, i) => equals(elem, arrR[i]))
+      );
+  }
+}
+
+const eq = native(
+  (loc, ...args): BoolValue => {
+    if (args.length < 2)
+      throw new LisaError("Expected 2 or more args to '='", loc);
+    const comparator = args[0][0];
+    return bool(args.slice(1).every(elem => equals(comparator, elem[0])));
+  }
+);
 
 export const stdlib = {
   notnone,
-  log
+  log,
+  "=": eq
 };
